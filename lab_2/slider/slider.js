0e0cefde4ba80f78b05b3c36e94360c6643e36d3
Vadim Sigutin <ma3x.kross@gmail.com>
2022-11-12 22:05:19 +0600

    feat: add save saving

--- lab_2/slider/slider.js
+++ lab_2/slider/slider.js
@@ -1,40 +1,25 @@
-const sliderLine = document.querySelector('.slider-line'),
-	sliders = document.querySelectorAll('.slider-item'),
-	prevButton = document.querySelector('.btn-prev'),
-	nextButton = document.querySelector('.btn-next'),
-	dots = document.querySelectorAll('.dot')
-let position = 0,
-	dotIndex = 0
+const sliderLine = document.querySelector('.slider-line')
+const width = sliderLine.parentElement.getBoundingClientRect().width
+const prevButton = document.querySelector('.btn-prev')
+const nextButton = document.querySelector('.btn-next')
+const dots = document.querySelectorAll('.dot')
+let dotIndex = parseInt(localStorage.dotIndex) || 0
+let position = parseInt(localStorage.position) || 0
+let direction = 'next'
 
-const nextSlide = () => {
-	if (position < (dots.length - 1) * 720) {
-		position += 720
-
-		dotIndex++
-		console.log(dotIndex)
-	} else {
-		position = 0
-		dotIndex = 0
+const moveSlide = (event) => {
+	if (event) {
+		direction = event.target === nextButton ? 'next' : 'prev'
 	}
-	// sliderLine.style.left = -position + 'px'
-	sliderLine.style.transform = `translateX(${-position}px)`
+	dotIndex += direction === 'next' ? 1 : -1
+	if (dotIndex > dots.length - 1) dotIndex = 0
+	if (dotIndex < 0) dotIndex = dots.length - 1
+	position += direction === 'next' ? -width : width
+	if (position <= -width * dots.length) position = 0
+	if (position > 0) position = -width * (dots.length - 1)
 	currentSlide(dotIndex)
 	clearInterval(myTimer)
-	myTimer = setInterval(nextSlide, 3000)
-}
-
-const prevSlide = () => {
-	if (position > 0) {
-		position -= 720
-		dotIndex--
-	} else {
-		position = (dots.length - 1) * 720
-		dotIndex = dots.length - 1
-	}
-	sliderLine.style.transform = `translateX(${-position}px)`
-	currentSlide(dotIndex)
-	clearInterval(myTimer)
-	myTimer = setInterval(prevSlide, 3000)
+	myTimer = setInterval(moveSlide, 3000)
 }
 
 const currentSlide = (index) => {
@@ -42,23 +27,25 @@ const currentSlide = (index) => {
 		dot.classList.remove('active')
 	}
 	dots[index].classList.add('active')
+
+	sliderLine.style.transform = `translateX(${position}px)`
+	localStorage.setItem('dotIndex', index)
+	localStorage.setItem('position', sliderLine.style.transform.match(/-?\d+/))
 }
 
-nextButton.addEventListener('click', nextSlide)
-prevButton.addEventListener('click', prevSlide)
+nextButton.addEventListener('click', moveSlide)
+prevButton.addEventListener('click', moveSlide)
 
 document.addEventListener('keydown', (event) => {
-	if (event.code == 'Space' || event.code == 'ArrowRight') nextSlide()
-	if (event.code == 'ArrowLeft') prevSlide()
+	if (event.code == 'Space' || event.code == 'ArrowRight') {
+		direction = 'next'
+		moveSlide()
+	}
+	if (event.code == 'ArrowLeft') {
+		direction = 'prev'
+		moveSlide()
+	}
 })
 
-dots.forEach((dot, index) => {
-	dot.addEventListener('click', () => {
-		position = 720 * index
-		sliderLine.style.left = -position + 'px'
-		dotIndex = index
-		currentSlide(dotIndex)
-	})
-})
-
-let myTimer = setInterval(nextSlide, 3000)
+currentSlide(dotIndex)
+let myTimer = setInterval(moveSlide, 3000)
