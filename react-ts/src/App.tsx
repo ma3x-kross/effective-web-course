18141cb70af8b16c691a0b570d584001ac0168be
Vadim Sigutin <ma3x.kross@gmail.com>
2022-11-26 20:04:48 +0600

    feat: add series details page

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -3,9 +3,12 @@ import { FC } from 'react';
 import { Outlet, RouteObject, useRoutes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import CharacterInfo from './routes/CharacterInfo';
 import Characters from './routes/Characters';
 import Comics from './routes/Comics';
+import ComicsInfo from './routes/ComicsInfo';
 import Series from './routes/Series';
+import SeriesInfo from './routes/SeriesInfo';
 
 const App: FC = () => {
   const routes: RouteObject[] = [
@@ -18,13 +21,25 @@ const App: FC = () => {
           element: <Characters />
         },
         {
+          path: '/:id',
+          element: <CharacterInfo />
+        },
+        {
           path: '/comics',
           element: <Comics />
+        },
+        {
+          path: '/comics/:id',
+          element: <ComicsInfo />
         },
         {
           path: '/series',
           element: <Series />
         },
+        {
+          path: '/series/:id',
+          element: <SeriesInfo />
+        }
       ]
     }
   ];
---
+++ react-ts/src/routes/SeriesInfo/SeriesInfo.tsx
@@ -0,0 +1,62 @@
+import { FC, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { CardProps } from '../../types/CardProps';
+
+import SERIES from '../../mocks/seriesData';
+import {
+  Box,
+  Container,
+  Divider,
+  Heading,
+  Image,
+  Link,
+  Text
+} from '@chakra-ui/react';
+
+const SeriesInfo: FC = () => {
+  const [seriesInfo, setPersonInfo] = useState<CardProps | null>(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      const numberId = parseInt(id, 10);
+      const result = SERIES.find((series) => series.id === numberId);
+      if (result) {
+        setPersonInfo(result);
+        console.log(seriesInfo);
+      }
+    }
+  }, [seriesInfo]);
+
+  return (
+    <>
+      {seriesInfo && (
+        <Container display="flex" maxW="container.xl" p={6}>
+          <Image
+            src={seriesInfo?.img}
+            w="395px"
+            h="600px"
+            mr={10}
+            borderRadius="xl"
+          />
+          <Box display="flex" flexDirection="column" gap="4">
+            <Heading>{seriesInfo.name}</Heading>
+            <Text mb={3}>{seriesInfo.description}</Text>
+            <Divider />
+            <Heading size="md">Related comics</Heading>
+            {seriesInfo.ref?.comics?.map((comics) => (
+              <Link href={`/comics/${comics.id}`}>{comics.name}</Link>
+            ))}
+            <Divider />
+            <Heading size="md">Related characters</Heading>
+            {seriesInfo.ref?.characters?.map((characters) => (
+              <Link href={`/${characters.id}`}>{characters.name}</Link>
+            ))}
+          </Box>
+        </Container>
+      )}
+    </>
+  );
+};
+
+export default SeriesInfo;
